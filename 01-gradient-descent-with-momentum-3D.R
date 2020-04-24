2604a2b670a48ea8a5cf16ee04934b8f7eaf0f97
Imran Kocabiyik <imrankocabiyik@gmail.com>
2020-04-24 10:17:22 +0200

    Remove av package

--- 01-gradient-descent-with-momentum-3D.R
+++ 01-gradient-descent-with-momentum-3D.R
@@ -1,18 +1,37 @@
 library(av)
+library(tidyverse)
+library(stringr)
+library(glue)
 
 # video specs ----
-width = 1920
+video_name = "gd-with-momentum2"
 height = 1080
-framerate = 24
-verbose = TRUE
-apply_momentum = FALSE
-output = "gd-with-momentum.mp4"
+framerate = 29
+duration_in_sec = 5
 
-# music ----
-big_horns_intro <- system.file("samples/Big_Horns_Intro.mp3",  package = "av", mustWork = TRUE)
-info <- av_video_info(big_horns_intro)
-len <- framerate * round(info$duration)
-res <- round(72 * min(width, height)/480)
+# gradient descent ----
+learning_rate <- 5e-04
+x_val <- 6
+y_val <- 6
+
+# momentum terms, if applicable
+apply_momentum = TRUE
+beta = 0.95
+vdx = 0
+vdy = 0
+
+# video generation settings ----
+verbose = TRUE
+plot_naming = "plot_%03d"
+
+# video specs auto calculated ----
+width = height*(16/9)
+num_iter <- duration_in_sec*framerate
+dir_name = str_c('video_', video_name)
+len = num_iter
+
+# directory settings ----
+if (!dir.exists(dir_name)) { dir.create(dir_name) }
 
 # test function ----
 f <- function(x, y) {
@@ -32,17 +51,6 @@ dx <- function(x, y) {
 dy <- function(x, y) {
     4 * y^3 + 2 * x^2 - 26 * y + 4 * x * y - 22
   }
-  
-# gradient descent ----
-num_iter <- len
-learning_rate <- 5e-04
-x_val <- 6
-y_val <- 6
-
-# momentum terms, if applicable
-beta = 0.95
-vdx = 0
-vdy = 0
 
 # initialize gd steps
 updates_x <- vector("numeric", length = num_iter)
@@ -70,49 +78,54 @@ for (i in 1:num_iter) {
   updates_z[i] <- z_val
 }
 
-# visualize before creating the video ---
-persp(x, y, z, theta = -50 - i * 0.05, 
-      phi = 20 + log(i), expand = 0.5, col = "lightblue", 
-      border = "lightblue", axes = FALSE, box = FALSE, 
-      ltheta = 100, shade = 0.9)
-points(
-  trans3d(updates_x[1:i],
-          updates_y[1:i], 
-          updates_z[1:i],
-          pmat = plt), pch = 16, cex = c(rep(0.6, i - 1), 1.2),
-  col = c(rep("white", i - 1), "black")
-)
 
 # animation ----
-video <- av_capture_graphics(output = output, audio = NULL, 
-                               {
-                                 for (i in seq_len(len)) {
-                                   
-                                   # verbose 
-                                   if (as.logical(verbose)) cat(sprintf("\rGenerating plot %d/%d...", i,  len), file = stderr())
-                                   
-                                   # plot the surface
-                                   plt <- persp(x, y, z, theta = -50 - i * 0.05, 
-                                                phi = 20 + log(i), expand = 0.5, col = "lightblue", 
-                                                border = "lightblue", axes = FALSE, box = FALSE, 
-                                                ltheta = 100, shade = 0.9)
-                                   
-                                   # adding points, representing gradient updates
-                                   points(
-                                     trans3d(updates_x[1:i],
-                                             updates_y[1:i], 
-                                             updates_z[1:i],
-                                             pmat = plt), pch = 16, cex = c(rep(0.6, i - 1), 1.2),
-                                     col = c(rep("white", i - 1), "black")
-                                     )
-                                   
-                                 }
-                                 
-                                 # verbose for success
-                                 if (as.logical(verbose)) cat("done!\n", file = stderr())
-                               }
-                             ,width = width, height = height, res = res, framerate = framerate, 
-                             )
-# find video ----
-utils::browseURL(video)
 
+for (i in seq_len(len)) {
+  
+  # verbose 
+  if (as.logical(verbose)) cat(sprintf("\rGenerating plot %d/%d...", i,  len), file = stderr())
+  
+  # plot the surface
+  plot_name <- sprintf(plot_naming, i)
+  png(glue('{dir_name}/{plot_name}.png'), width = width, height = height)
+  
+  plt <- persp(x, y, z, theta = -50 - i * 0.05, 
+               phi = 20 + log(i), expand = 0.5, col = "lightblue", 
+               border = "lightblue", axes = FALSE, box = FALSE, 
+               ltheta = 100, shade = 0.9)
+  
+  # adding points, representing gradient updates
+  n_back = 20
+  start_n = i
+  
+  if (i<n_back) {
+    start_n = 1
+    point_colors = c(rep("white", i-1), "black")
+    point_sizes = c(seq(1, 2, length.out = n_back-1), 2.5)
+    point_sizes = tail(point_sizes, i)
+    point_transparency = c(seq(from = 0.1, to = 0.9, length.out = n_back-1),1)
+    point_transparency = tail(point_transparency)
+  } else {
+    start_n = i-n_back+1
+    point_colors = c(rep("white", n_back-1), "black")
+    point_sizes = c(seq(0.1, 2, length.out = n_back-1), 2.5)
+    point_transparency = c(seq(from = 0.1, to = 0.9, length.out = n_back-1),1)
+  }
+  points(
+    trans3d(updates_x[start_n:i],
+            updates_y[start_n:i], 
+            updates_z[start_n:i],
+            pmat = plt), pch = 16, cex = point_sizes,
+    col = point_colors,
+    alpha = point_sizes
+    )
+  dev.off()
+  
+}
+
+# verbose for success
+if (as.logical(verbose)) cat("done!\n", file = stderr())
+
+cmd <- glue('ffmpeg  -framerate 10 -i {plot_naming}.png -s:v {width}x{height} -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p -r 30 {video_name}.mp4')
+cmd
