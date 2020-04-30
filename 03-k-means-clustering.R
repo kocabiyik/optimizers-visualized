dc41f29a59957adc0e1369ff11b57049cd16426a
Imran Kocabiyik <imrankocabiyik@gmail.com>
2020-04-30 22:20:02 +0200

    Add magnets to visualizations

--- 03-k-means-clustering.R
+++ 03-k-means-clustering.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+
+# 0. generate data ----
 n = 5
 
 X = tibble(x1=sample(1:10, n),
@@ -11,58 +13,67 @@ X = tibble(x1=sample(1:10, n),
   as.matrix()
 
 # placeholder
-centroids = X[1:n,]
+initial_centroids = X[1:n,]
 
 # Set K
 K = n;
 
-# You need to return the following variables correctly.
-idx = rep(0, nrow(X));
-
-nrow = nrow(X);
-
-for (i in 1:nrow) {
-  x1 = X[i,1]
-  x2 = X[i,2]
-  for (j in 1:K) {
-    # centroid points
-    c1 = centroids[j,1]
-    c2 = centroids[j,2]
+# 1.1 functions: find closest centroids ----
+find_closest_centroids <- function(X, centroids) {
   
-    # initialize
-    if (j == 1) {
-      idx[i] = 1; 
-      dist_cache = sqrt((x2-c2)^2+(x1-c1)^2);
-    }
-    
-    # calculate distance
-    calculated_dist = sqrt((x2-c2)^2+(x1-c1)^2);
-    
-    # update centroid point
-    if (calculated_dist<dist_cache) {
-      dist_cache = calculated_dist; 
-      idx[i] = j
+  # placeholder
+  idx = rep(0, nrow(X));
+  
+  for (i in 1:length(idx)) {
+    x1 = X[i,1]
+    x2 = X[i,2]
+    for (j in 1:K) {
+      # centroid points
+      c1 = centroids[j,1]
+      c2 = centroids[j,2]
+      
+      # initialize
+      if (j == 1) {
+        idx[i] = 1; 
+        dist_cache = sqrt((x2-c2)^2+(x1-c1)^2);
+      }
+      
+      # calculate distance
+      calculated_dist = sqrt((x2-c2)^2+(x1-c1)^2);
+      
+      # update centroid point
+      if (calculated_dist<dist_cache) {
+        dist_cache = calculated_dist; 
+        idx[i] = j
+      }
     }
   }
+  return(idx)
 }
 
-# visualize
-library(tidyverse)
+centroids <- find_closest_centroids(X, initial_centroids)
+
+# 2. visualize ----
 df <- tibble(x1=X[,1],
            x2=X[,2],
-           centroid = as.factor(idx)
+           centroid = as.factor(centroids)
            )
 
-df_centroids = tibble(centroid_x1 = centroids[,1],
-                      centroid_x2 = centroids[,2],
-                      centroid = as.factor(1:n))
+df_centroids = tibble(centroid_x1 = initial_centroids[,1],
+                      centroid_x2 = initial_centroids[,2],
+                      centroid = as.factor(1:n)) %>% 
+  mutate(img = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/whatsapp/238/magnet_1f9f2.png")
 
 df %>% left_join(df_centroids) %>% 
-  ggplot(aes(x1,x2, color = centroid))+
-  geom_point()+
+  ggplot(aes(x1,x2))+
+  geom_point(aes(color = centroid))+
   geom_segment(aes(x=x1,
                    xend=centroid_x1,
                    y=x2,
-                   yend=centroid_x2
+                   yend=centroid_x2,
+                   color = centroid
                    ),
-               alpha = 0.3)+theme_void()
+               alpha = 0.3)+theme_void()+
+  geom_image(data = df_centroids, aes(image=img, x=centroid_x1, y=centroid_x2), size = 0.025)+
+  coord_fixed()+
+  theme(legend.position = "none")
